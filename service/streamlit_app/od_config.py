94bdce885ac4960782fcd61f7ef55f7506ce64f2
Alexander Shchelokov <edgyestlord@gmail.com>
2024-03-30 23:12:45 +0700

    fix  colours in frontend config

--- service/streamlit_app/od_config.py
+++ service/streamlit_app/od_config.py
@@ -2,8 +2,38 @@ MIN_CONFIDENCE_THRESHOLD = 0.0
 MAX_CONFIDENCE_THRESHOLD = 1.0
 DEFAULT_CONFIDENCE_THRESHOLD = 0.3
 
-DEFAULT_CLASSES_TO_INCLUDE = ['Cardiomegaly', 'Aortic enlargement', 'ILD', 'Infiltration', 'Lung Opacity']
-CLASS_COLOURS = ["red", "green", "blue", "yellow", "cyan"]
+DEFAULT_CLASSES_TO_INCLUDE = [
+    "Aortic enlargement",
+    "Atelectasis",
+    # "Calcification",
+    "Cardiomegaly",
+    # "Consolidation",
+    "ILD",
+    "Infiltration",
+    "Lung Opacity",
+    # "Nodule/Mass",
+    # "Pleural effusion",
+    # "Pleural thickening",
+    # "Pneumothorax",
+    # "Pulmonary fibrosis"
+]
+
+# Assign colors to each label
+CLASS_COLOURS = [
+    "red",
+    "green",
+    "blue",
+    "purple",
+    "orange",
+    "cyan",
+    # "magenta",
+    # "yellow",
+    # "pink",
+    # "brown",
+    # "lime",
+    # "teal",
+    # "olive"
+]
 CLASS_COLOUR_DICT = dict(zip(DEFAULT_CLASSES_TO_INCLUDE, CLASS_COLOURS))
 
 API_ENDPOINT = 'http://127.0.0.1:4242/detect_objects'
